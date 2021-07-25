856604866207522156ea92eb2848edf1d2ce1502
lostoy <raymond007.lee@gmail.com>
2021-07-26 02:05:43 +0800

    add publish config and test code

--- sample/src/main/java/com/lostoy/android/monitor/sample/MainActivity.kt
+++ sample/src/main/java/com/lostoy/android/monitor/sample/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.lostoy.android.monitor.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lostoy.android.monitor.R
+import android.os.SystemClock
+import androidx.appcompat.app.AppCompatActivity
+import com.lostoy.android.monitor.Config
+import com.lostoy.android.monitor.Monitor
+import com.lostoy.android.monitor.Switches
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Monitor.init(Config(switches = Switches(true)))
+        Monitor.monitor(
+            "onCreate", mapOf(
+                "pageName" to MainActivity::javaClass.name,
+                "timestamp" to SystemClock.elapsedRealtime().toString()
+            )
+        )
+        Monitor.addExtraParams(
+            mapOf(
+                "timestamp" to SystemClock.elapsedRealtime().toString(),
+                "userId" to "88888888"
+            )
+        )
     }
 }
